7836e3d94f96899c91d2e4af012c25b0da39bcc6
Yifan Qiu <yif.chu22@gmail.com>
2023-02-17 18:42:05 +0000

    average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,11 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double sum = 0;
+        for(double num: nums){
+            sum+=num;
+        }
+        double res = sum/nums.length;
+        return res;
     }
 }
